cb109a43633fe528e8778eb8a46f02ac982eb244
Rahul Chandawat <rahulchandawat22aug@gmail.com>
2022-04-06 16:27:28 +0530

    Time: 86 ms (62.24%), Space: 48.3 MB (91.90%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+
+    // bool makeJump(vector<int>& nums, int curr, int n){
+    //     if(curr>=(n-1))
+    //         return true;
+    //     if(nums[curr]<=0)
+    //         return false;
+
+    //     int x=nums[curr];
+
+    //     while(x>=1){
+    //        bool result=makeJump(nums, curr+x,n); 
+    //        if(result==true)
+    //         return true;
+    //         x--;
+    //     }
+
+    //     return false;
+
+    // }
+
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)
+            return true;
+        if(nums[0]==0)
+            return false;
+
+        int requiredJump=1;
+        int i=n-2;
+        while(i>=0){
+            if(nums[i]>=requiredJump){
+                requiredJump=1;
+                i--;
+            }
+            else{
+                requiredJump++;
+                i--;
+            }
+        }
+        if(nums[0]>=requiredJump){
+            return true;
+        }
+        return false;
+        // return makeJump(nums, 0, n);
+
+
+    }
+};
