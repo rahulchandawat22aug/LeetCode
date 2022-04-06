a59d81d75c231adf19860812a2021b8b4c3324be
Rahul Chandawat <rahulchandawat22aug@gmail.com>
2022-04-06 14:49:16 +0530

    Time: 164 ms (34.50%), Space: 67.8 MB (12.10%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -12,11 +12,12 @@ public:
 
         for(int i=0;i<n;i++){
             sum+=nums[i];
-            if(sum < nums[i]){
-                sum=nums[i];
-            }
             if(sum > max)
                 max=sum;
+
+            if(sum < 0){
+                sum=0;
+            }
         }
         return max;
     }
