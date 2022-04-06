ef8f7e50170e4a89bd8e03799d8664e014576134
Rahul Chandawat <rahulchandawat22aug@gmail.com>
2022-04-06 17:01:17 +0530

    Time: 80 ms (70.51%), Space: 48.5 MB (35.99%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -3,11 +3,6 @@ public:
 
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        if(n==1)
-            return true;
-        if(nums[0]==0)
-            return false;
-
         int requiredJump=0, i=n-2;
         while(i>=0){
             requiredJump++;
@@ -15,9 +10,9 @@ public:
                 requiredJump=0;
             i--;        
         }
-        if(requiredJump==0){
+        if(requiredJump==0)
             return true;
-        }
+
         return false;
     }
 };
