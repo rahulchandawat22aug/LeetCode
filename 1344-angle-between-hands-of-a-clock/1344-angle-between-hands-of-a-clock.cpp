9fb7a574d3e68cc9c14a5fd9231a84ad56bc5430
Rahul Chandawat <rahulchandawat22aug@gmail.com>
2022-05-05 00:31:09 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (77.64%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    double angleClock(int h, int m) {
-        if(h==12 && m==0){
-            return 0;
-        }
-        double aph, apm;
-        apm=6;
-        aph=0.5;
-        double ah, am;
-        am=apm*m;
-        ah=aph*m+h*30;
-        
-        double ans=abs(am-ah);
-        return min(ans, 360-ans);
-        
-        // return 0;
+double angleClock(int hour, int minutes) {
+        double num_hours = (30*((double)hour + ((double)(minutes)/60)));  // total time in hours eg 12:30 == 12.5 hours
+		//and multiplying with 30 as 1 hour = 30 degrees 
+        if(num_hours > 360) {
+            num_hours -= 360; 
+        }// If the result goes above 360 
+        double ans_min = 6 * minutes;  // every min is 6 degrees 
+        double result = abs(num_hours - ans_min);  // This is first answer but the minimal 
+		//could be 360 - answer. Thats why the below line  :) 
+        return min(result, 360 - result);
     }
+	// If you understood it then please Upvote , it motivates me !!
 };
