108bbf13e752b7cec4175712f484986d0eeeee0c
Rahul Chandawat <rahulchandawat22aug@gmail.com>
2022-10-11 02:52:31 +0530

    Time: 1913 ms (5.02%), Space: 8.2 MB (8.57%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+
+    vector<vector<int>> dir{{0,1}, {1,0}, {-1,0}, {0,-1}};
+
+    bool exist(vector<vector<char>>& board, string word, int crow, int ccol, int sindex, vector<vector<int>> &vi) {
+        
+        int len_word = word.size();
+        int nrow = board.size();
+        int ncol = board[0].size();
+        
+        // if(sindex == len_word-1 && word[sindex]==board[crow][ccol])
+        //     return true;
+        
+        if(sindex == len_word)
+            return true;
+        
+        if(crow >= nrow || ccol >= ncol || crow < 0 || ccol < 0 || sindex < 0 || vi[crow][ccol] == 1 || (board[crow][ccol] != word[sindex]))
+            return false;
+        // cout<<crow<<" "<<ccol<<" "<<sindex<<" "<<endl;
+
+        vi[crow][ccol] = 1;
+        for(int i=0;i<4;i++){
+            bool temp = exist(board, word, crow+dir[i][0], ccol+dir[i][1], sindex+1, vi);
+            if(temp == true){
+                return true;
+            }
+        }
+        vi[crow][ccol] = 0;
+        return false;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int len_word = word.size();
+        int nrow = board.size();
+        int ncol = board[0].size();
+
+        if(len_word == 0){
+            return true;
+        }
+        vector<vector<int>> vi(nrow, vector<int> (ncol, 0));
+        
+        for(int i=0;i<nrow;i++){
+            for(int j=0;j<ncol;j++){
+               if(board[i][j] == word[0]){
+                   bool temp = exist(board, word, i, j, 0, vi);
+                   if(temp == true){
+                    return true;
+                   }                
+               }
+ 
+            }
+        }
+
+        return false;
+    }
+};
